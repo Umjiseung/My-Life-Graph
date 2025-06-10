2f97f4362c2c2c6a35bb95a0ea0f49bd0c6b0676
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-06-10 17:48:29 +0900

    init

---
+++ src/main/kotlin/webpro/mylifegraph/MyLifeGraphApplication.kt
@@ -0,0 +1,11 @@
+package webpro.mylifegraph
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyLifeGraphApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyLifeGraphApplication>(*args)
+}
---
+++ src/test/kotlin/webpro/mylifegraph/MyLifeGraphApplicationTests.kt
@@ -0,0 +1,13 @@
+package webpro.mylifegraph
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyLifeGraphApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
