e66d138662bdffc81c81ef954c5f5e3bf32d8321
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-06-11 18:20:29 +0900

    Add User Entity

---
+++ src/main/kotlin/webpro/mylifegraph/user/persistence/User.kt
@@ -0,0 +1,18 @@
+package webpro.mylifegraph.user.persistence
+
+import jakarta.persistence.*
+import java.util.UUID
+
+@Entity
+class User(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(nullable = false)
+    val name: String,
+
+    @Column(nullable = false, unique = true)
+    val uniqueId: UUID
+)
