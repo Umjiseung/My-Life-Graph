bb0925b43297d849e417aeed1e3b9c2ba293505d
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2025-06-26 09:40:25 +0900

    Setting SSL

--- src/main/kotlin/webpro/mylifegraph/MyLifeGraphApplication.kt
+++ src/main/kotlin/webpro/mylifegraph/MyLifeGraphApplication.kt
@@ -3,6 +3,7 @@ package webpro.mylifegraph
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
 class MyLifeGraphApplication
 
